6b238bb22f839edfe1afd7523fe7e2b46ef3ca94
Sushree Sadhana Khatua <82105572+sushreekhatua@users.noreply.github.com>
2022-02-01 23:28:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hi"+" sadhana");
+            var man='D';
+            Console.WriteLine(man);
+            List<int>sadhana=new List<int>{34,76,89};
+            foreach (var item in sadhana)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
