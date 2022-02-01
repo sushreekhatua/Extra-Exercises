1c64dafd118099b87b9d714c6d27864688f2b489
Sushree Sadhana Khatua <82105572+sushreekhatua@users.noreply.github.com>
2022-02-01 23:37:44 +0100

    Add files via upload

---
+++ CarSpeedLimit/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to enter the speed limit.
+            System.Console.Write("Enter the speed limit of car: ");
+            int speedlimit=int.Parse(Console.ReadLine());
+           
+
+            //Ask for the speed of a car
+            System.Console.Write("Enter the speed of car: ");
+            int carspeed=int.Parse(Console.ReadLine());
+            
+
+            System.Console.WriteLine($"\nThe speed limit for car is {speedlimit} km/h.");
+            System.Console.WriteLine($"The speed of car is {carspeed} km/h.");
+
+            /*If the user enters a value less than the speed limit, 
+            program should display Ok on the console. If the value is above the speed limit,
+             the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 
+             1 demerit points should be incurred and displayed on the console. 
+            If the number of demerit points is above 12, the program should display License Suspended.
+            */
+            calculatespeed(carspeed,speedlimit);
+        }
+
+        public static void calculatespeed(int carspeed,int speedlimit)
+        {
+            if (carspeed< speedlimit)
+            {
+                System.Console.WriteLine("OK");
+            }
+            else
+            {    
+                int diff=carspeed-speedlimit;
+                int demeritpoint=diff/5;
+                System.Console.WriteLine("The total number of demeritpoint is :{0}",demeritpoint);
+                if(demeritpoint>12)
+                {
+                    System.Console.WriteLine("License Suspended!");
+                }
+                System.Console.WriteLine("Your speed is not ok.Drive Slowly.");
+
+            }
+        }
+    }
+}
---
+++ ConsecutiveNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Write a program and ask the user to enter a few numbers separated by a hyphon. 
+Workout if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-17-16",
+ display a message: "Consecutive"; otherwise, display "Not-Consecutive".
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsecutiveNum // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a few numbers separated by a hyphon.\n");
+            var x=Console.ReadLine();
+            var list=x.Split("-").Select(x => int.Parse(x.Trim())).ToList();
+            System.Console.WriteLine("The elements in the list are :");
+            foreach(var i in list)
+            {
+                System.Console.WriteLine(i);
+            }
+            string smthng="I don't know";
+            
+            for(int i=1;i<(list.Count);i++)
+            {
+                if(list[i]+1==list[i-1] || list[i]-1==list[i-1])
+                {
+                    smthng="Consecutive";
+                }
+                else
+                {
+                    smthng="Non-consecutive";
+                    i=list.Count;
+                }
+            }
+            System.Console.WriteLine($"The series you entered are ({smthng}) numbers!!");
+            
+        }
+    }
+}
